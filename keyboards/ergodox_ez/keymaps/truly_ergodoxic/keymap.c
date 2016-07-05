ed4fce8bc7a6828f411ae0f649c725bed5d717df
Nate Eagleson <nate@nateeag.com>
2016-07-05 08:05:09 -0400

    Revert "Move truly_ergodoxic Plover pinky keys down a row"
    This reverts commit 4a2bd02efc310918c4b246bf8c44bc60f85d6125.
    
    When I actually started using it, this turned out to not work as well as
    I'd hoped.

--- keyboards/ergodox_ez/keymaps/truly_ergodoxic/keymap.c
+++ keyboards/ergodox_ez/keymaps/truly_ergodoxic/keymap.c
@@ -84,28 +84,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,      KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          KC_RSFT,
         KC_BSPC,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_QUOT,          F(TE_CTL_ESC),
-                             KC_RGUI,KC_DOWN,KC_LBRC,KC_RBRC,          KC_RALT,
+                             KC_RGUI,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_RALT,
              TG(1),       MO(2),
              KC_UP,
              KC_DOWN,KC_ENT, KC_SPC
     ),
 
-/* Keymap 1: Steno for Plover, adapted from
- * https://github.com/shayneholmes/tmk_keyboard/commit/11290f8489013018f778627db725160c745e75bd
- *
- * I've shifted the pinky keys down by one row because that seems to fit my
- * hands better.
+/* Keymap 1: Steno for Plover from https://github.com/shayneholmes/tmk_keyboard/commit/11290f8489013018f778627db725160c745e75bd
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |   2  |   3  |   4  |   5  |      |           |      |  6   |  7   |   8  |   9  |      |        |
+ * |        |   1  |   2  |   3  |   4  |   5  |      |           |      |  6   |  7   |   8  |   9  |  0   |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   1  |   w  |   e  |   r  |   t  |------|           |------|  y   |  u   |   i  |   o  |  0   |   [    |
+ * |        |   q  |   w  |   e  |   r  |   t  |------|           |------|  y   |  u   |   i  |   o  |  p   |   [    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   q  |   s  |   d  |   f  |   g  |      |           |      |  h   |  j   |   k  |   l  |  p   |   '    |
+ * |        |   a  |   s  |   d  |   f  |   g  |      |           |      |  h   |  j   |   k  |   l  |  ;   |   '    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |   a  |      |      |      |                                       |      |      |      |  ;   |      |
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -118,20 +114,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [PLVR] = KEYMAP(  // layout: layer 1: Steno for Plover
         // left hand
-        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-        KC_NO,  KC_NO,  KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
-        KC_NO,  KC_1,   KC_W,   KC_E,   KC_R,   KC_T,
-        KC_NO,  KC_Q,   KC_S,   KC_D,   KC_F,   KC_G,   KC_NO,
-        KC_NO,  KC_A,   KC_NO,  KC_NO,  KC_NO,
+        KC_NO, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+        KC_NO,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
+        KC_NO,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,
+        KC_NO,  KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_NO,
+        KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
                                       KC_FN4, KC_NO,
                                            KC_NO,
                                  KC_C,   KC_V,   KC_NO,
         // right hand
         KC_TRNS,  KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_NO,   KC_TRNS,
-        KC_NO,    KC_6,    KC_7,    KC_8,   KC_9,    KC_NO,   KC_TRNS,
-                  KC_Y,    KC_U,    KC_I,   KC_O,    KC_0,    KC_LBRC,
-        KC_NO,    KC_H,    KC_J,    KC_K,   KC_L,    KC_P,    KC_QUOT,
-                           KC_TRNS, KC_TRNS,KC_NO,   KC_SCLN,   KC_NO,
+        KC_NO,    KC_6,    KC_7,    KC_8,   KC_9,    KC_0,    KC_TRNS,
+                  KC_Y,    KC_U,    KC_I,   KC_O,    KC_P,    KC_LBRC,
+        KC_NO,    KC_H,    KC_J,    KC_K,   KC_L,    KC_SCLN, KC_QUOT,
+                           KC_TRNS, KC_TRNS,KC_NO,   KC_NO,   KC_NO,
         KC_TRNS, KC_TRNS,
         KC_TRNS,
         KC_NO,  KC_N,   KC_M
