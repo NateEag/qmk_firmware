3e8b4265ca0c0592deb790285f9add801539b74e
Nate Eagleson <nate@nateeag.com>
2022-03-05 12:12:30 -0500

    Add brightness up/down to truly_ergodoxic layout

--- keyboards/ergodox_ez/keymaps/truly_ergodoxic/keymap.c
+++ keyboards/ergodox_ez/keymaps/truly_ergodoxic/keymap.c
@@ -250,9 +250,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        | Rclk |MsLeft|MsDown|MsRght| Lclk |------|           |------|      | VolDn| VolUp| Play | Stop |        |
  * |--------+------+------+------+------+------| Del  |           | Del  |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |        |      |BrtUp |BrtDwn|      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |CADel |      |      |      |      |                                       |      |    . |      |      |      |
+ *   |CADel |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -268,7 +268,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,           KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_INS,
        KC_TRNS, KC_TRNS, KC_MS_WH_DOWN, KC_MS_U, KC_MS_WH_UP, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN1,
-       KC_TRNS,           KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_DEL,
+       KC_TRNS,           KC_TRNS,KC_BRIU,KC_BRID,KC_TRNS,KC_TRNS,KC_DEL,
        LALT(LCTL(KC_DEL)),KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_HOME,
